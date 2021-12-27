8ee781ae244665660dbd92777c7ec09575514c73
tinsaemesfin <qmesfin@gmail.com>
2021-12-27 17:14:12 +0300

    after isadmin is fixed mid of ep2/29

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,68 +16,8 @@
 </head>
 
 <body>
-    <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
-        <div class="sidebar-brand d-none d-md-flex">
-            <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-                <use xlink:href="{{ asset('assets/brand/coreui.svg#full') }}"></use>
-            </svg>
-            <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-                <use xlink:href="{{ asset('assets/brand/coreui.svg#signet') }}"></use>
-            </svg>
-        </div>
-        <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="init">
-            <div class="simplebar-wrapper" style="margin: 0px;">
-                <div class="simplebar-height-auto-observer-wrapper">
-                    <div class="simplebar-height-auto-observer"></div>
-                </div>
-                <div class="simplebar-mask">
-                    <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
-                        <div class="simplebar-content-wrapper" style="height: 100%; overflow: hidden scroll;">
-                            <div class="simplebar-content" style="padding: 0px;">
-                                <li class="nav-item"><a class="nav-link" href="index.html">
-                                        <svg class="nav-icon">
-                                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
-                                        </svg> Dashboard<span class="badge badge-sm bg-info ms-auto">NEW</span></a></li>
-
-                                <li class="nav-title">Components</li>
-                                <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
-                                        <svg class="nav-icon">
-                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-                                        </svg> Base</a>
-                                    <ul class="nav-group-items">
-                                        <li class="nav-item"><a class="nav-link"
-                                                href="base/accordion.html"><span class="nav-icon"></span>
-                                                Accordion</a></li>
-
-                                    </ul>
-                                </li>
-                                <li class="nav-item"><a class="nav-link" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        <svg class="nav-icon">
-                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-                                        </svg>  {{ __('Logout') }}</a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                </li>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="simplebar-placeholder" style="width: auto; height: 840px;"></div>
-            </div>
-            <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;">
-                <div class="simplebar-scrollbar" style="width: 0px; display: none;"></div>
-            </div>
-            <div class="simplebar-track simplebar-vertical" style="visibility: visible;">
-                <div class="simplebar-scrollbar"
-                    style="height: 250px; transform: translate3d(0px, 8px, 0px); display: block;"></div>
-            </div>
-        </ul>
-        <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
-    </div>
+    
+    @include('partials.sidebar')
 
     {{-- HEADER --}}
     <div class="wrapper d-flex flex-column min-vh-100 bg-light">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PageController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware'=>'auth'], function(){
+    Route::group(['prefix'=>'admin','as' => 'admin.','middleware'=>'is_admin'], function(){
+    Route::resource('pages',PageController::class);
+    });
+});
